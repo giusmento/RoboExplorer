b0ba3c4b5acd3dbffa0084016091226a64d91b22
giuseppemento <mento.giuseppe@gmail.com>
2020-08-21 13:54:03 +0200

    first commit

--- library/motor/RoboMotor.py
+++ library/motor/RoboMotor.py
@@ -1,8 +1,24 @@
-from enum import Enum
+from gpiozero import Motor, OutputDevice
 
+class RoboMotor:
+    def __init__(self, enable, positive, negative):
+        self.motor: Motor = Motor(positive, negative, pwm=False)
+        self.motorEnable:OutputDevice = OutputDevice(enable, active_high=0)
 
-class RoboMotor(Enum):
-    ONE = 0
-    TWO = 1
-    THREE = 2
-    FOUR =3
+    def forward(self, speed=1):
+        self.motor.forward(speed)
+
+    def stop(self):
+        self.motor.stop()
+
+    def backward(self, speed=1):
+        self.backward(speed)
+
+    def enable(self):
+        self.motorEnable.on()
+
+    def disable(self):
+        self.motorEnable.off()
+
+    def is_active(self):
+        return self.motor.is_active
--- runner/main.py
+++ runner/main.py
@@ -1,5 +1,6 @@
 import logging
 from logging.config import fileConfig
+import argparse
 from os import path
 import time
 import sys
@@ -8,12 +9,9 @@ import os
 import gpiozero
 from gpiozero.pins.mock import MockFactory
 
-gpiozero.Device.pin_factory = MockFactory()
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-from library.motor.RoboMotor import RoboMotor
-from library.motor import robo_motors
+from library.motor.motors import RobotMotorOne
 
 log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging_config.ini')
 logging.config.fileConfig(log_file_path)
@@ -24,9 +22,17 @@ if __name__ == '__main__':
 
     logger.info('Application just started.')
 
-    #robo_motors.enable_robomotor(RoboMotor.ONE)
-    robo_motors.move_forward(1, RoboMotor.ONE)
-    time.sleep(1)
-    robo_motors.stop_robomotor(RoboMotor.ONE)
+    parser = argparse.ArgumentParser(description='Exercise')
+    parser.add_argument('--mock_gpio', type=bool, nargs='?', const=True, default=False, help='Mock gpio', required=False)
 
+    args = parser.parse_args()
+    MOCK_GPIO = args.test if args.test != None else False
+
+    if MOCK_GPIO:
+        gpiozero.Device.pin_factory = MockFactory()
+
+    
+    RobotMotorOne.forward()
+    time.sleep(5)
+    RobotMotorOne.stop()
     logger.info('exit!')
